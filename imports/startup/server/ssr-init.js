75395a4606a0d8a990055e37d67c6b20b1086e2b
Minh Nguyen <minhna@gmail.com>
2017-12-13 23:16:13 +0700

    issue #8, react-helmet is working

--- imports/startup/server/ssr-init.js
+++ imports/startup/server/ssr-init.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { onPageLoad } from 'meteor/server-render';
 import { StaticRouter } from 'react-router';
+import { Helmet } from "react-helmet";
+
 import routes from '../both/routes.js';
 
 onPageLoad((sink) => {
@@ -14,4 +16,8 @@ onPageLoad((sink) => {
   );
 
   sink.renderIntoElementById('app', renderToString(<App location={sink.request.url} />));
+
+  const helmet = Helmet.renderStatic();
+  sink.appendToHead(helmet.meta.toString());
+  sink.appendToHead(helmet.title.toString());
 });
