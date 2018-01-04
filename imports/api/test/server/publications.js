b48a81903bb7f87230234dff1fd45991924d7f5c
Minh Nguyen <minhna@gmail.com>
2018-01-04 22:13:16 +0700

    add sample of publish without reactive

--- imports/api/test/server/publications.js
+++ imports/api/test/server/publications.js
@@ -19,3 +19,21 @@ Meteor.publish('tests.mine', function () {
 
   return Tests.find(query, options);
 });
+
+Meteor.publish('tests.mine.noreactive', function () {
+  if (!this.userId) {
+    return this.ready();
+  }
+
+  const query = {
+    owner: this.userId,
+  };
+
+  const options = {
+    fields: { createdAt: 0 },
+  };
+
+  const tests = Tests.find(query, options);
+  tests.forEach(test => this.added('tests', test._id, test));
+  this.ready();
+});
