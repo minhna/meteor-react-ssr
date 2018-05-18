d9cd7505e09ae7ce8add4b7510e6c8b4de5f2f49
Minh Nguyen <minhna@gmail.com>
2018-05-18 11:54:08 +0700

    remove materialize meteor package, use npm instead.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,8 +1,16 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import { onPageLoad } from 'meteor/server-render';
+
+if (Meteor.isClient) {
+  import Materialize from 'materialize-css';
+  // set global (that way we don´t need to import in every file)
+  global.M = Materialize;
+  global.Materialize = Materialize;
+}
 
 const history = createHistory();
 
