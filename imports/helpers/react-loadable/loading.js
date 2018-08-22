7838fcff67aef1ff70f35caaa453f311f4f9a0af
Minh Nguyen <minhna@gmail.com>
2018-08-22 10:38:20 +0700

    impove loading component

--- imports/helpers/react-loadable/loading.js
+++ imports/helpers/react-loadable/loading.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Loading = (props) => {
-  // console.log(props);
-  if (props.error) {
-    return <div>Error!</div>;
-  } else if (props.timedOut) {
+  const {
+    error, timedOut, pastDelay, retry,
+  } = props;
+  if (error) {
+    return (
+      <div>
+        {`Error! ${error.message || ''} `}
+        <button type="button" onClick={() => { retry(); }}>Retry</button>
+      </div>
+    );
+  }
+  if (timedOut) {
     return <div>Taking a long time...</div>;
-  } else if (props.pastDelay) {
+  }
+  if (pastDelay) {
     return <div>Loading...</div>;
   }
 
   return null;
+};
+
+Loading.propTypes = {
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+  timedOut: PropTypes.bool,
+  pastDelay: PropTypes.bool,
+  retry: PropTypes.func,
+};
+
+Loading.defaultProps = {
+  error: null,
+  timedOut: false,
+  pastDelay: false,
+  retry: () => {},
 };
 
 export default Loading;
